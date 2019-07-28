a732e1d5a75b366dd2a2cf0e3011d65cd5bc4e06
carospark <52773045+carospark@users.noreply.github.com>
2019-07-28 22:39:14 +1000

    Update 5. 5yr_windows_composited.R

--- sea_analysis/5. 5yr_windows_composited.R
+++ sea_analysis/5. 5yr_windows_composited.R
@@ -1,7 +1,3 @@
-#from may 27 redo regional analysis
-
-rm(list=ls())
-setwd("~/Desktop/Dir/github")
 
 load("/Users/awesomesauce/Desktop/Dir/lisa/abs_estimates_new.Rda")
 load("/Users/awesomesauce/Desktop/Dir/lisa/country_groups.Rda")
@@ -13,48 +9,30 @@ ay <- filter(country_groups, code %in% wanted)
 ay$code <- as.character(ay$code)
 abs <- left_join(abs_estimates_new, ay, by="iso")
 
-
 abs <- abs[complete.cases(abs),]
-
-View(abs)
-
 abs[,2:6]= (abs[,2:6]-abs$avg)/abs$sd
 abs$iso <- NULL
 res<- abs %>% group_by(code.x, code.y) %>% summarise_each(mean)
 
-boop <- res[,1:7]
-View(boop)
+res2 <- res[,1:7]
+colnames(res2)<- c("code", "group", "0", "-2", "-1", "+1", "+2")
+res2[res2 == "5801"] <- "Least Developed Countries"
+res2[res2 == "5802"] <- "Landlocked Developing"
+res2[res2 == "5815"] <- "Low Income Food Deficit"
+res2[res2 == "5817"] <- "Net Food-Importing Developing"
+res2[res2 == "5706"] <- "European Union"
 
-colnames(boop)<- c("code", "group", "0", "-2", "-1", "+1", "+2")
-boop[boop == "5801"] <- "Least Developed Countries"
-boop[boop == "5802"] <- "Landlocked Developing"
-boop[boop == "5815"] <- "Low Income Food Deficit"
-boop[boop == "5817"] <- "Net Food-Importing Developing"
-boop[boop == "5706"] <- "European Union"
+res2 <- as.data.frame(res2)
+res2 <- reshape::melt(res2, by=c("code", "group"))
 
-boop <- as.data.frame(boop)
-boop2 <- reshape::melt(boop, by=c("code", "group"))
-View(boop2)
+res2$variable <- factor(res2$variable, levels = c("-2", "-1", "0", "+1", "+2"))
+res2<- arrange(res2, code)
 
-boop2$variable <- factor(boop2$variable, levels = c("-2", "-1", "0", "+1", "+2"))
-
-
-# boop2$grpee <- paste(boop2[,1],boop2[,2])
-boop2<- arrange(boop2, code)
-
-
-
-head(global)
 
 global <- abs_estimates_new
-
 global[,2:6]= (global[,2:6]-global$avg)/global$sd
-
 global$iso <- NULL
 global2<- global %>% group_by(code) %>% summarise_each(mean)
-
-global2
-
 
 
 
