6c1f78e6e39e3bcb40801c84bbb1fd982ea00b38
carospark <52773045+carospark@users.noreply.github.com>
2019-07-28 22:18:34 +1000

    Update 1. multiyear_events.R

--- sea_analysis/1. multiyear_events.R
+++ sea_analysis/1. multiyear_events.R
@@ -1,34 +1,7 @@
-
-#from may1 recreating popdam not averag
-
-
-rm(list=ls())
-
-
-#-----------------------------------------------------------------------------------------------------------------------------------------------
-
-load("~/Desktop/Dir3/e'z2/emdat_combined.Rda")
-emdat_orig <- emdat
-load("~/Desktop/Dir/resubmission/emdat_newpopdams.Rda")
-
-
-colnames(emdat)
-
-colnames(emdat_orig)
-
-emdat_wna <- emdat_orig[,-c(4:5)]
-colnames(emdat_wna) <- c("start_year", "distype", "iso", "totalaff", "totaldam")
-
-save(emdat_wna, file="emdat_w_nas.Rda")
-
-
-#------------------------------------------------------------------
-
+#load in EWE data
 load("~/Desktop/Dir/resubmission/emdat_w_nas.Rda")
-emdat <- emdat_wna
 
-#load("~/Desktop/Dir/resubmission/new_popdam.Rda")
-
+# function to find EWEs that occurred in the same country over a consecutive period of years
 find_consecutive_years = function(years) {
   groups = split(years, cumsum(c(1, diff(years) != 1)))
   temp = sapply(groups, function(x) {
@@ -52,39 +25,18 @@ a3_new = ddply(.data = emdat,
                  return(df)
                })
 
-View(a3_new)
-
-a3_new$start_year <- as.numeric(a3_new$start_year)
-a3_new$end_year <- as.numeric(a3_new$end_year)
-
 a32b <- a3_new %>% mutate(duration= end_year-start_year)
-#a32b <- filter(a32b, duration<=5)
-#View(a32b)
 
-View(a1)
-a1<- filter(a32b, duration==0)
-a1$start_year <- as.numeric(as.character(a1$start_year))
-blah2 <- left_join(a1, emdat, by=c("iso", "start_year", "distype"))
-View(blah2)
-blah2$duration=NULL
-
-
-#------------------------------------------------------------------------------------
-
-
-anti_join(a3, a3_new, by=c("iso", "distype", "start_year"))
-
-
+#separate EWEs by single events or multiyear events 
+single <- filter(a32b, duration==0)
+single2 <- left_join(single, emdat, by=c("iso", "start_year", "distype"))
 
 multiyear <- filter(a32b, duration>0)
-
-#View(multiyear)
 multiyear <- multiyear[,1:4]
 
+#for multiyear events, extract each year in order to aggregate total "persons affected" and total "financial damage" sums
 temp <- NULL
 temp <- data.frame()
-# #View(temp)
-# 
 for(i in 1:nrow(multiyear)){
   x = as.data.frame(rbind(c(multiyear$iso[i], multiyear$distype[i], (multiyear$start_year[i]:multiyear$end_year[i]))))
   temp <- rbind.fill(temp,x)
@@ -98,17 +50,12 @@ temp$variable <- NULL
 
 colnames(temp) <- c("iso", "distype", "start_year")
 temp <- filter(temp, !is.na(start_year))
-
-#View(temp)
-temp$start_year <- as.numeric(as.character(temp$start_year))
 temp <- arrange(temp, iso, distype, start_year)
 
 temp2<- left_join(temp, emdat, by=c("iso", "distype", "start_year"))
-
-View(temp2)              
 temp2 <- arrange(temp2, iso, distype, start_year)
 
-rmp <- temp2 %>% 
+multiyear2 <- temp2 %>% 
   group_by(iso, distype, cumsum(c(1, diff(temp2$start_year) != 1))) %>%
   summarize(sumaff = sum(totalaff, na.rm=TRUE),
             sumdam = sum(totaldam, na.rm=TRUE),
@@ -116,15 +63,13 @@ rmp <- temp2 %>%
             end_year1 = max(start_year)
   )
 
-colnames(rmp) <- c("iso", "distype", "cumsum", "sumaff", "sumdam", "start_year", "end_year")
-View(rmp)
+colnames(multiyear2) <- c("iso", "distype", "cumsum", "sumaff", "sumdam", "start_year", "end_year")
 
-View(multiyear)
-#------------------------------------------------------
-
+#load in population and GDP data (from World Bank)
 load("~/Desktop/Dir/resubmission/pop2.Rda")
 load("~/Desktop/Dir/resubmission/gdp2.Rda")
 
+#find and append population and GDP for the country and year of each multiyear EWE
 temptemp <- left_join(temp, pop2,  by=c("iso", "start_year")) %>% left_join(., gdp2,  by=c("iso", "start_year"))
 View(temptemp)
 temptemp <- arrange(temptemp, iso, distype, start_year)
@@ -137,39 +82,25 @@ temptemp <- temptemp %>% group_by(iso, distype, cumsum(c(1, diff(temptemp$start_
 
 temptemp$`cumsum(c(1, diff(temptemp$start_year) != 1))`<- NULL
 colnames(temptemp) <- c("iso", "distype", "pop", "gdp", "start_year", "end_year")
+multiyear_total <- left_join(multiyear2, multiyear, by=c("iso", "distype", "start_year", "end_year")) %>% left_join(., temptemp,  by=c("iso", "distype", "start_year", "end_year"))
 
-#------------------------------------------------------
-
-blah <- left_join(rmp, multiyear, by=c("iso", "distype", "start_year", "end_year")) %>% left_join(., temptemp,  by=c("iso", "distype", "start_year", "end_year"))
-View(blah)
-
-#THIS IS WHERE YOU CAN CHOOSE TO NOT AVERAGE!
-
-blah$duration = (blah$end_year-blah$start_year)+1
-blah$totalaff = blah$sumaff/blah$duration
-blah$totaldam = blah$sumdam/blah$duration
-
-blah <- blah[, -c(3,10)]
-#blah <- dplyr::rename(blah, distype=distype.x)
-#blah <- dplyr::rename(blah, totaldam = meantotaldam, totalaff= meantotalaff)
+#append population and GDP for single-year EWEs
+single2 <- single2 %>% group_by(iso, start_year, end_year, distype) %>% summarise(totaldam = sum(totaldam), totalaff=sum(totalaff))
+single2 <- left_join(single2, pop2, by=c("iso", "start_year")) %>% left_join(., gdp2, by=c("iso", "start_year"))
 
 
-blah2 <- blah2 %>% group_by(iso, start_year, end_year, distype) %>% summarise(totaldam = sum(totaldam), totalaff=sum(totalaff))
+#non-averaged total sums for "persons affected" and "financial damage"
+multiyear_total$duration = (multiyear_total$end_year-multiyear_total$start_year)+1
+multiyear_total$totalaff = multiyear_total$sumaff/multiyear_total$duration
+multiyear_total$totaldam = multiyear_total$sumdam/multiyear_total$duration
 
-blah2 <- left_join(blah2, pop2, by=c("iso", "start_year")) %>% left_join(., gdp2, by=c("iso", "start_year"))
+multiyear_total <- multiyear_total[, -c(3,10)]
 
 
-b3 <- bind_rows(blah, blah2)
-View(b3)
+#commbine all EWEs and find the proportion of affected per population, or financial damage per GDP
+combined <- bind_rows(multiyear_total, single2)
+combined$ppop <- (combined$totalaff/combined$pop)*100
+combined$pgdp <- ((combined$totaldam*1000)/combined$gdp)*100
 
-filta <- b3
-View(filta)
-
-filta$ppop <- (filta$totalaff/filta$pop)*100
-
-#turn off scientific notation
-options(scipen=999)
-
-filta$pgdp <- ((filta$totaldam*1000)/filta$gdp)*100
-save(filta, file="alldis_alldata.Rda")
+save(combined, file="alldis_alldata.Rda")
 
